cf1f15e36efea0c13c7ce7e1f886ff5be6295dcf
angryWilly <kirill.teyxrib@yandex.ru>
2024-04-12 00:18:28 +0700

    init existing project

---
+++ Assets/CodeBase/Infrastructure/IState.cs
@@ -0,0 +1,19 @@
+﻿namespace CodeBase.Infrastructure
+{
+    public interface IState : IExitableState
+    {
+        public void Enter();
+    }
+
+    public interface IPayLoadedState<TPayLoad> : IExitableState
+    {
+        public void Enter(TPayLoad sceneName);
+    }
+    
+    public interface IExitableState
+    {
+        public void Exit();
+    }
+    
+
+}
