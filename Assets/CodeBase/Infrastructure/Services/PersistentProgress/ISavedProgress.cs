2325edc2287aa126f5c49d6a1646b5ecaf4d64be
angryWilly <kirill.teyxrib@yandex.ru>
2024-04-14 18:36:02 +0700

    data and new state for save and load

---
+++ Assets/CodeBase/Infrastructure/Services/PersistentProgress/ISavedProgress.cs
@@ -0,0 +1,14 @@
+﻿using CodeBase.Data;
+
+namespace CodeBase.Infrastructure.Services.PersistentProgress
+{
+    public interface ISavedProgressRider
+    {
+        void LoadProgress(PlayerProgress progress);
+    }
+
+    public interface ISavedProgress : ISavedProgressRider
+    {
+        void UpdateProgress(PlayerProgress progress);
+    }
+}
