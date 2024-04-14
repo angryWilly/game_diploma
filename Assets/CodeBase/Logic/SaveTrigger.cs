1f7113cef4c722e46794d0e41a13b59b318036c3
angryWilly <kirill.teyxrib@yandex.ru>
2024-04-14 19:34:45 +0700

    new dependencies for LoadLevelState.cs and save trigger prefab

---
+++ Assets/CodeBase/Logic/SaveTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using CodeBase.Infrastructure.Services;
+using CodeBase.Infrastructure.Services.SaveLoad;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace CodeBase.Logic
+{
+    public class SaveTrigger : MonoBehaviour
+    {
+        private ISaveLoadService _saveLoadService;
+        [SerializeField] private BoxCollider _collider;
+        
+        private void Awake()
+        {
+            _saveLoadService = AllServices.Container.Single<ISaveLoadService>();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            _saveLoadService.SaveProgress();
+            Debug.Log("Progress Saved.");
+            gameObject.SetActive(false);
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (!_collider)
+                return;
+            
+            Gizmos.color = new Color32(30, 200, 30, 130);
+            Gizmos.DrawCube(transform.position + _collider.center, _collider.size);
+        }
+    }
+}
