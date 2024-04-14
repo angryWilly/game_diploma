566854e1d060ae42b8283c2c7c9601d4ca878bf8
angryWilly <kirill.teyxrib@yandex.ru>
2024-04-14 19:34:45 +0700

    new dependencies for LoadLevelState.cs and save trigger prefab

--- Assets/CodeBase/Infrastructure/States/LoadLevelState.cs
+++ Assets/CodeBase/Infrastructure/States/LoadLevelState.cs
@@ -1,5 +1,6 @@
 ﻿using CodeBase.CameraLogic;
 using CodeBase.Infrastructure.Factory;
+using CodeBase.Infrastructure.Services.PersistentProgress;
 using CodeBase.Logic;
 using UnityEngine;
 
@@ -13,13 +14,15 @@ namespace CodeBase.Infrastructure.States
         private readonly SceneLoader _sceneLoader;
         private readonly LoadingCurtain _curtain;
         private readonly IGameFactory _gameFactory;
+        private readonly IPersistentProgressService _progressService;
 
-        public LoadLevelState(GameStateMachine stateMachine, SceneLoader sceneLoader, LoadingCurtain curtain, IGameFactory gameFactory)
+        public LoadLevelState(GameStateMachine stateMachine, SceneLoader sceneLoader, LoadingCurtain curtain, IGameFactory gameFactory, IPersistentProgressService progressService)
         {
             _stateMachine = stateMachine;
             _sceneLoader = sceneLoader;
             _curtain = curtain;
             _gameFactory = gameFactory;
+            _progressService = progressService;
         }
         
         public void Enter(string sceneName)
@@ -34,13 +37,25 @@ namespace CodeBase.Infrastructure.States
 
         private void OnLoaded()
         {
+            InitGameWorld();
+            InformProgressReaders();
+
+            _stateMachine.Enter<GameLoopState>();
+        }
+
+        private void InformProgressReaders()
+        {
+            foreach (ISavedProgressReader progressReader in _gameFactory.ProgressReaders)
+                progressReader.LoadProgress(_progressService.Progress);
+        }
+
+        private void InitGameWorld()
+        {
             GameObject hero = _gameFactory.CreateHero(at: GameObject.FindWithTag(InitialPointTag));
 
             _gameFactory.CreateHud();
-            
+
             CameraFollow(hero);
-            
-            _stateMachine.Enter<GameLoopState>();
         }
 
         private static void CameraFollow(GameObject hero)
