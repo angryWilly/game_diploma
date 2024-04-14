46652363718899577bdd831d400e016afcf1c918
angryWilly <kirill.teyxrib@yandex.ru>
2024-04-14 17:20:29 +0700

    singleton for all services and refactor

--- Assets/CodeBase/Infrastructure/States/BootstrapState.cs
+++ Assets/CodeBase/Infrastructure/States/BootstrapState.cs
@@ -1,24 +1,34 @@
-﻿using CodeBase.Services.Input;
+﻿using CodeBase.Infrastructure.AssetManagement;
+using CodeBase.Infrastructure.Factory;
+using CodeBase.Infrastructure.Services;
+using CodeBase.Services.Input;
 using UnityEngine;
 
-namespace CodeBase.Infrastructure
+namespace CodeBase.Infrastructure.States
 {
     public class BootstrapState : IState
     {
         private const string Initial = "Initial";
         private readonly GameStateMachine _stateMachine;
         private readonly SceneLoader _sceneLoader;
+        private readonly AllServices _services;
 
-        public BootstrapState(GameStateMachine stateMachine, SceneLoader sceneLoader)
+        public BootstrapState(GameStateMachine stateMachine, SceneLoader sceneLoader, AllServices services)
         {
             _stateMachine = stateMachine;
             _sceneLoader = sceneLoader;
+            _services = services;
+            
+            RegisterServices();
         }
         
         public void Enter()
         {
-            RegisterServices();
             _sceneLoader.Load(Initial, onLoaded: EnterLoadLevel);
+        }
+
+        public void Exit()
+        {
         }
 
         private void EnterLoadLevel() => 
@@ -26,14 +36,12 @@ namespace CodeBase.Infrastructure
 
         private void RegisterServices()
         {
-            Game.InputService = RegisterInputService();
+            _services.RegisterSingle<IInputService>(InputService());
+            _services.RegisterSingle<IAssets>(new AssetsProvider());
+            _services.RegisterSingle<IGameFactory>(new GameFactory(_services.Single<IAssets>()));
         }
 
-        public void Exit()
-        {
-        }
-
-        private static IInputService RegisterInputService()
+        private static IInputService InputService()
         {
             if (Application.isEditor)
                 return new StandaloneInputService();
