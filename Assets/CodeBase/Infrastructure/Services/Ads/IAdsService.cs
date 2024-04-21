5daa58559b628ff60e154a3cfcc205b9022e2272
angryWilly <kirill.teyxrib@yandex.ru>
2024-04-21 16:44:43 +0700

    add ads service

---
+++ Assets/CodeBase/Infrastructure/Services/Ads/IAdsService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CodeBase.Infrastructure.Services.Ads
+{
+    public interface IAdsService : IService
+    {
+        event Action RewardedVideoReady;
+        void Initialize();
+        void ShowRewardedVideo(Action onVideoFinished);
+    }
+}
